4051b9dbeffb8c12ac1dfa2daaa10db4268224b3
Ashlant <95947089+Ashlant@users.noreply.github.com>
2023-03-09 15:41:31 -0800

    Create studentTest.java

---
+++ Assignment 2/Problem 1/studentTest.java
@@ -0,0 +1,10 @@
+public class studentTest {
+	public static void main(String[] args) {
+		Student student1 = new Student("John", "CS", 3.5);
+		Student student2 = new Student();
+		student2.setName("Mary Ann");
+		student2.setMajor("CE");
+		student2.setGpa(3.3);
+		System.out.print(student1.toString() + "\n" + student2.toString());
+	}
+}

aed7f2370738c474713e5f6c80f4b6291eee8fbf
Ashlant <95947089+Ashlant@users.noreply.github.com>
2023-03-12 22:13:56 -0700

    Update Player.java

--- Assignment 2/Problem 5/c/Player.java
+++ Assignment 2/Problem 5/c/Player.java
@@ -1,6 +1,6 @@
 public class Player {
-	private String name;
-  private boolean expert;
+  	private String name;
+  	private boolean expert;
 	
 	public Player(String name, boolean expert) {
 		this.name = name;

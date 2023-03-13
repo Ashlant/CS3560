aa45f687e4c658e9eb752fca15905116400883c0
Ashlant <95947089+Ashlant@users.noreply.github.com>
2023-03-12 21:44:27 -0700

    Create Book.java

---
+++ Assignment 2/Problem 5/b/Book.java
@@ -0,0 +1,18 @@
+public class Book {
+	private String name;
+	private String author;
+	private Course course;
+	
+  public Book() {
+    //...
+  }
+	public Book(String name, String author) {
+		this.name = name;
+		this.author = author;
+	}
+	public Book(String name, String author, Course course) {
+		this.name = name;
+		this.author = author;
+		this.course = course;
+	}
+}

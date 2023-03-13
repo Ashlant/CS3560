7abc2d4f0ccef7c8fcfe8e09d4eb2f9dfc4f7685
Ashlant <95947089+Ashlant@users.noreply.github.com>
2023-03-12 23:10:19 -0700

    Create Person.java

---
+++ Assignment 2/Problem 5/g/Person.java
@@ -0,0 +1,11 @@
+public class Person {
+  private String name;
+  private Watch watch;
+  private Movie movie;
+
+  public Person(String name, Watch watch, Movie movie) {
+    this.name = name;
+    this.watch = watch;
+    this.movie = movie;
+  }
+}

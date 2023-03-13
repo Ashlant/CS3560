9f06e5304514684c12b7111297d5fc4367441413
Ashlant <95947089+Ashlant@users.noreply.github.com>
2023-03-12 22:25:29 -0700

    Update Student.java

--- Assignment 2/Problem 5/a/Student.java
+++ Assignment 2/Problem 5/a/Student.java
@@ -2,9 +2,6 @@ public class Student {
   private String name;
   private Transcript transcript;
   
-  public Student(String name) {
-	  this.name = name;
-  }
   public Student(String name, Transcript transcript) {
 	  this.name = name;
 	  this.transcript = transcript;

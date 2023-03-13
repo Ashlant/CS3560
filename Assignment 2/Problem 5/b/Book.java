1a708c8d1b6f05e47220043986f8b4e79a3fe263
Ashlant <95947089+Ashlant@users.noreply.github.com>
2023-03-12 22:09:21 -0700

    Update Book.java

--- Assignment 2/Problem 5/b/Book.java
+++ Assignment 2/Problem 5/b/Book.java
@@ -3,16 +3,7 @@ public class Book {
 	private String author;
 	private Course course;
 	
-  public Book() {
-    //...
-  }
-	public Book(String name, String author) {
-		this.name = name;
-		this.author = author;
-	}
-	public Book(String name, String author, Course course) {
-		this.name = name;
-		this.author = author;
-		this.course = course;
-	}
+  	public Book() {
+		//...
+  	}
 }

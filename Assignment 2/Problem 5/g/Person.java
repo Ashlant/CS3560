a6212d196ada3e23d97133c291cedcfd5aade1c8
Ashlant <95947089+Ashlant@users.noreply.github.com>
2023-03-12 23:17:26 -0700

    Update Person.java

--- Assignment 2/Problem 5/g/Person.java
+++ Assignment 2/Problem 5/g/Person.java
@@ -1,11 +1,9 @@
 public class Person {
   private String name;
   private Watch watch;
-  private Movie movie;
 
-  public Person(String name, Watch watch, Movie movie) {
+  public Person(String name, Watch watch) {
     this.name = name;
     this.watch = watch;
-    this.movie = movie;
   }
 }
